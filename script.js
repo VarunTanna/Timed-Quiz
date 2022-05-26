4e18f61a957453534633dfe2c44b97e847a2ab65
VarunTanna <varuntanna@Varuns-MacBook-Pro.local>
2022-05-25 23:47:51 -0400

    html almost done and starting Js

---
+++ script.js
@@ -0,0 +1,65 @@
+// grab all the elements 
+var viewhighscores = document.getElementById("viewhighscore");
+var timer = document.getElementById("time");
+var start = document.getElementById("startquiz");
+var startbtn = document.getElementById("startButton");
+var quiz = document.getElementById("startQuiz");
+var questions = document.getElementById("quetion");
+var buttonA = document.getElementById("a");
+var buttonB = document.getElementById("b");
+var buttonC = document.getElementById("c");
+var buttonD = document.getElementById("d");
+var result =
+var gameover =
+var finalScore =
+var intials =
+var score =
+var ontainer =
+var highscorePage =
+var highscoreHeader =
+var highscoreIntials =
+var highscoreScore =
+var endGameBtns =
+var playAgain =
+var clearhighScore =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // event listener on the start quiz.
+
+//hide that and then show questions inside a function
+
+//event listener on all answer buttons. only add one.
+
+// seprate function to change the question to the next question 
+
+//// compare the buttton they pick to the right answer
+
+//set up timer last
+
+// take the time they have left as their score
+
+// save that as their high score in local storage
+
+// key and the score saved to local storage
+
+// no page manu other than hidding and unhiding things 
+
